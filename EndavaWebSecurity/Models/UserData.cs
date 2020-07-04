ad2ce8c7964c4b2940e8fc56d65de64ee610ffcc
Geogre Dumbrava <rewolutionlife@gmail.com>
2020-07-04 12:57:04 +0300

    Form with JS Validation

---
+++ EndavaWebSecurity/Models/UserData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EndavaWebSecurity.Models
+{
+    public class UserData
+    {
+        string Name { get; set; }
+        string LastName { get; set; }
+        string Email { get; set; }
+        string Password{ get; set; }
+    }
+}
